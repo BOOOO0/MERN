ba8820850b196b7d52356d1a5ba2558200b355d1
Buyoung Chung <56213742+qndud0206@users.noreply.github.com>
2022-06-28 21:31:38 +0900

    220628

--- models/User.js
+++ models/User.js
@@ -13,6 +13,10 @@ const userSchema = mongoose.Schema({
     //trim 공백 없애줌
     trim: true,
   },
+  password: {
+    type: String,
+    minlength: 5,
+  },
   lastname: {
     type: String,
     maxlength: 50,
@@ -30,20 +34,22 @@ const userSchema = mongoose.Schema({
   },
 });
 // pre()는 mongoose에서 가져온 메소드
-// userSchema.pre("save", function (next) {
-//   //비밀번호를 암호화 시킨다
-//   var user = this;
-//   if (user.isModified("password")) {
-//     bcrypt.genSalt(saltRounds, function (err, salt) {
-//       if (err) return next(err);
-//       bcrypt.hash(user.password, salt, function (err, hash) {
-//         if (err) return next(err);
-//         user.password = hash;
-//         next();
-//       });
-//     });
-//   }
-// });
+userSchema.pre("save", function (next) {
+  //비밀번호를 암호화 시킨다
+  var user = this;
+  // password가 바뀔때만 암호화가 일어나게 만든다 이것을 하지 않으면
+  // email이나 다른 것을 바꿀 때도 암호화가 또 일어난다
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
