873e3d4f61d78e95e8c631d894998d3ec9373f01
Buyoung Chung <56213742+qndud0206@users.noreply.github.com>
2022-06-29 20:02:42 +0900

    login token

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 const config = require("./config/key");
-
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,13 +32,45 @@ app.get("/", (req, res) => {
 app.post("/register", (req, res) => {
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터베이스에 넣어준다
+
   const user = new User(req.body);
   // save() 는 mongoDB에서 오는 메소드
+  // 여기가 password 암호화의 next()
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     //status200은 성공했다는 뜻
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //첫번째 요청된 이메일을 데이터베이스에 있는지 찾는다
+  //findOne은 mongoDB 메소드
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당되는 유저가 없습니다.",
+      });
+    }
+    //두번째 요청된 이메일이 데이터베이스에 있다면 비밀번호 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      //비밀번호까지 맞다면 토큰을 생성하기
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        //토큰을 저장한다 쿠키, 로컬스토리지
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
--- models/User.js
+++ models/User.js
@@ -3,6 +3,8 @@ const bcrypt = require("bcrypt");
 //salt를 생성한 다음 그것을 이용해서 비밀번호 암호화
 //saltRounds 는 salt가 몇글자인지를 정하는것
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -48,8 +50,29 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
+  } else {
+    next();
   }
 });
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword 1234567       암호화된 비밀번호 (복호화를 할 수 없으니 plainPassword를 암호화해서 비교)
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  //jsonwebtoken을 이용해서 token을 생성하기
+  //user._id가 plainobject가 아니였기 때문에 toHexString()을 사용
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
